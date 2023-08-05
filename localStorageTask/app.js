ac900258f5324eb4020fbb51ce6c2bc44da189e4
kobrak1 <burakkarhan657@gmail.com>
2023-08-06 02:39:43 +0300

    todo list modifid

--- localStorageTask/app.js
+++ localStorageTask/app.js
@@ -1,2 +1,11 @@
-localStorage.setItem("burak", "25")
-localStorage.clear()
+
+
+let myArray = [{name:"burak", age:35}, {name:"hasan", age:23}];
+myArray = JSON.stringify(myArray)
+me = JSON.parse(myArray)
+
+
+let pf = document.getElementById("print").innerHTML = myArray[3] +"<br>" + me[1].name
+                                                   
+
+
